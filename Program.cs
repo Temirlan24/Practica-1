4f6395ce51059694acde752318704f170c900bdd
Temirlan24 <52044447+Temirlan24@users.noreply.github.com>
2019-06-26 14:49:51 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FIO: Yerzatuly Temirlan");
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            int number1 = Convert.ToInt32(a);
+            int number2 = Convert.ToInt32(b);
+            
+            Console.WriteLine(number1+number2);
+            Console.ReadKey();
+
+        }
+    }
+}
